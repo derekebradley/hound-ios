86749c4f54c2eb6dfb52c79ed39bbc9d40ec46e1
Derek Bradley <derekbradley@Derek-Bradleys-MacBook-Air.local>
2012-10-16 19:23:13 -0700

    Placeholder for the Drinks View Controller.

---
+++ Hound/DrinksVC.h
@@ -0,0 +1,13 @@
+//
+//  DrinksVC.h
+//  Hound
+//
+//  Created by Derek Bradley on 10/16/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DrinksVC : UIViewController
+
+@end
---
+++ Hound/DrinksVC.m
@@ -0,0 +1,38 @@
+//
+//  DrinksVC.m
+//  Hound
+//
+//  Created by Derek Bradley on 10/16/12.
+//
+//
+
+#import "DrinksVC.h"
+
+@interface DrinksVC ()
+
+@end
+
+@implementation DrinksVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
