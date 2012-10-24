d9c87ecdfa9626471849316dbf0ea8e25b0da8f8
Derek Bradley <derekbradley@Derek-Bradleys-MacBook-Air.local>
2012-10-23 21:11:00 -0700

    Added Drink Images to DrinksVC

--- Hound/DrinksVC.h
+++ Hound/DrinksVC.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface DrinksVC : UIViewController
+@interface DrinksVC : UIViewController <UITableViewDataSource, UITableViewDelegate>
 
 @end
--- Hound/DrinksVC.m
+++ Hound/DrinksVC.m
@@ -7,6 +7,7 @@
 //
 
 #import "DrinksVC.h"
+#import "UIImageView+AFNetworking.h"
 
 @interface DrinksVC ()
 
@@ -29,8 +30,10 @@
     self.view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 640)];
     
     self.tableView = [[UITableView alloc] initWithFrame:self.view.frame];
-    self.tableView.autoresizingMask = UIViewAutoresizingFlexibleHeight   |
+    self.tableView.autoresizingMask = UIViewAutoresizingFlexibleHeight  |
     UIViewAutoresizingFlexibleWidth;
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
     [self.view addSubview:self.tableView];
 }
 
@@ -45,5 +48,102 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 3;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+//    static NSString *CellIdentifier = @"Cell";
+//    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    UITableViewCell *cell;
+    
+    cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:nil];
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    
+    NSURL *url = nil;
+    
+    if (indexPath.row == 0)
+    {
+        url = [NSURL URLWithString:@"http://24.media.tumblr.com/tumblr_maj73brwmH1ro3h6zo1_1280.jpg"];
+    } else if (indexPath.row == 1)
+    {
+        url = [NSURL URLWithString:@"http://sphotos-b.xx.fbcdn.net/hphotos-ash3/c0.0.403.403/p403x403/643984_881386677004_1737261116_n.jpg"];
+    } else if (indexPath.row == 2)
+    {
+        url = [NSURL URLWithString:@"http://24.media.tumblr.com/tumblr_mazya7AYSs1ro3h6zo1_1280.jpg"];
+    }
+             
+    [cell.imageView setImageWithURL:url];
+    
+    return cell;
+}
+
+-(float)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 400;
+}
+
+/*
+ // Override to support conditional editing of the table view.
+ - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+ {
+ // Return NO if you do not want the specified item to be editable.
+ return YES;
+ }
+ */
+
+/*
+ // Override to support editing the table view.
+ - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+ {
+ if (editingStyle == UITableViewCellEditingStyleDelete) {
+ // Delete the row from the data source
+ [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+ }
+ else if (editingStyle == UITableViewCellEditingStyleInsert) {
+ // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+ }
+ }
+ */
+
+/*
+ // Override to support rearranging the table view.
+ - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+ {
+ }
+ */
+
+/*
+ // Override to support conditional rearranging of the table view.
+ - (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+ {
+ // Return NO if you do not want the item to be re-orderable.
+ return YES;
+ }
+ */
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Navigation logic may go here. Create and push another view controller.
+    /*
+     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
+     // ...
+     // Pass the selected object to the new view controller.
+     [self.navigationController pushViewController:detailViewController animated:YES];
+     */
+}
+
 
 @end
