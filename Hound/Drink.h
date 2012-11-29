056b5a103b6fcbf124aa7bcb75ca0d5133629c3d
Katie Smillie <katiesmillie@gmail.com>
2012-11-29 10:35:44 -0800

    [Drink] Add time, location, and user properties to model.

--- Hound/Drink.h
+++ Hound/Drink.h
@@ -7,11 +7,16 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "User.h"
 
 @interface Drink : NSObject
 
 @property (nonatomic, strong) NSString *imageUrl;
 @property (nonatomic, strong) NSString *name;
+@property (nonatomic, strong) NSString *time;
+@property (nonatomic, strong) NSString *location;
+@property (nonatomic, strong) User *user;
+
 
 + (NSArray *)recentDrinks;
 
